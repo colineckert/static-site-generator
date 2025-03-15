23f3f37fd8f3fb5339722c152dd03921e50e568f
Colin Eckert <colineckert10@gmail.com>
2025-03-15 08:52:09 -0700

    create textnode class and type enum

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+from textnode import TextNode, TextType
+
+def main():
+    print("Hello, World!")
+    textnode = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
+    print(textnode.__repr__())
+
+main()
