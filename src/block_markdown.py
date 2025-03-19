1f863d7f78c9981c7eb7e170d1c9cddb41755df5
Colin Eckert <colineckert10@gmail.com>
2025-03-19 04:50:56 -0700

    convert markdown to blocks and blocktypes

---
+++ src/block_markdown.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
+def markdown_to_blocks(markdown):
+    blocks = []
+    for section in markdown.split('\n\n'):
+        stripped = section.strip()
+        if stripped == "":
+            continue
+        lines = stripped.split('\n')
+        new_lines = []
+        for line in lines:
+            new_lines.append(line.strip())
+        block = '\n'.join(new_lines)
+        blocks.append(block)
+    return blocks
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
